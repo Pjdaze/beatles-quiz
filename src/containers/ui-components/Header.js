b41684adf0ad17efd91cadaf4ce15f7c646cd9e7
Pedro Diaz <pjdux@ilianas-MacBook-Pro.local>
2020-08-03 10:37:42 -0400

    added some styles

--- src/containers/ui-components/Header.js
+++ src/containers/ui-components/Header.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div style={{ padding: "20px", border: "5px solid #222" }}>
-      Beatles Quiz
+    <div style={{ padding: "20px", boxShadow: "0px 0px 5px #666" }}>
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "300px",
+          margin: "0 auto",
+        }}
+      >
+        <NavLink to="/Home">Home</NavLink>
+        <NavLink to="/Easy Test">Easy Test</NavLink>
+        <NavLink to="/HardTest">Hard Test</NavLink>
+      </nav>
     </div>
   );
 };
