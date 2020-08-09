8eb351d5c46e7a0b756f4a362908d8d00704e22e
Pjdaze <pjdazeux@gmail.com>
2020-08-09 15:58:09 -0400

    s

--- src/containers/Views/HardTest.js
+++ src/containers/Views/HardTest.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FormWrap from "../styled-components/FormWrap";
+import { Label, Select, Input } from "semantic-ui-react";
+import { RadioGroup, RadioButton } from "react-radio-buttons";
 
 const HardTest = () => {
-  return <FormWrap />;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://opentdb.com/api.php?amount=10&category=26";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(...data.results);
+        console.log(data.results[0]);
+      });
+  }, []);
+
+  return (
+    <FormWrap>
+      <Label className="labelx">
+        <p> {data.length && [data].map((x) => x[1].question)}</p>
+      </Label>
+      <RadioGroup className="radio-group">
+        <RadioButton
+          iconInnerSize={22}
+          rootColor="#222"
+          iconSize={22}
+          value="Alan Civil"
+        >
+          Alan Civil
+        </RadioButton>
+        <RadioButton
+          rootColor="#222"
+          iconInnerSize={22}
+          iconSize={22}
+          value="Arturo Sandoval"
+        >
+          Arturo Sandoval
+        </RadioButton>
+        <RadioButton
+          iconInnerSize={22}
+          rootColor="#222"
+          iconSize={20}
+          value="William May Stepher"
+        >
+          William May Stepher
+        </RadioButton>
+      </RadioGroup>
+    </FormWrap>
+  );
 };
 
 export default HardTest;
